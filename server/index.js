d6df0ad1bd6db5305941cd1f6ace920edf79d4e6
Victor <victorgab0802@gmail.com>
2025-03-12 20:01:52 -0300

    inicializando e organizando pastas do projeto

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+ const app = express();
+
+app.get("/", (req,ress) => {
+    return {"info": "Hello World!"}
+});
+
+app.listen(5000, () => console.log("Express started at http://127.0.0.1:5000"));
