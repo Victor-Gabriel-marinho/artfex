5df149e4b3dc751e2c3ceddcf6816be78d92011b
mauricio oliveira da costa <maumau.olliver@gmail.com>
2025-03-25 11:01:57 -0300

    header iniciado

--- server/front-end/src/App.js
+++ server/front-end/src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        
+        <header>
+            <nav>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li>Menu</li>
+                </ul>
+                <ul>
+                  <li><a href="#">Parceiros</a></li>
+                  <li>Mais</li>
+                </ul>
+            </nav>
+        </header>
     </div>
   );
 }
-
 export default App;
