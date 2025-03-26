7a32409172e872daca61c9d3e8027ab88ed35024
Victor <victorgab0802@gmail.com>
2025-03-26 12:40:45 -0300

    testando rotas e alterando nomes

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
         <header>
             <nav>
                 <ul>
-                  <li><a href="#">Home</a></li>
+                  <li><a href="/">Home</a></li>
                   <li>Menu</li>
                 </ul>
                 <ul>
-                  <li><a href="#">Parceiros</a></li>
+                  <li><a href="/">Parceiros</a></li>
                   <li>Mais</li>
                 </ul>
             </nav>
