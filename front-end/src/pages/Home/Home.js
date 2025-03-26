63c9d8f00d29374ac57e179cd1dec35191693fa7
Victor <victorgab0802@gmail.com>
2025-03-26 12:13:28 -0300

    organizando as pastas

---
+++ front-end/src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+        <header>
+            <nav>
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li>Menu</li>
+                </ul>
+                <ul>
+                  <li><a href="#">Parceiros</a></li>
+                  <li>Mais</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Home
