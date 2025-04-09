f1f41f6761e085ead210ffb3104e686155cfee33
Jose Ilcle <joseilclemaia@gmail.com>
2025-04-09 08:09:13 -0300

    funções de add e delete

--- server/controllers/user_commands.js
+++ server/controllers/user_commands.js
@@ -15,3 +15,33 @@ export const pegar_dados = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
 };
+
+// Rota para criar um novo usuário
+
+export const criar_usuario = async (req, res) => {
+  const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  const { nome, email, senha, cidade, estado } = req.body;
+
+  try {
+    const { data, error } = await supabase.from("usuarios").insert([{ nome, email, senha, cidade, estado }]);
+    if (error) throw error;
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Rota para deletar um usuário
+
+export const deletar_usuaria = async(req, res) => {
+  const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+  const { nome, email, senha, cidade, estado } = req.body;
+
+  try{
+    const {data, error} = await supabase.from("usuarios").delete().eq("id", id)
+    if (error) throw error
+    res.json(data)
+  } catch (error){
+    res.status(500).json({ error : error.message})
+  }
+}
