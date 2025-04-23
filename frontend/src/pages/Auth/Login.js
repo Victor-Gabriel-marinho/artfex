263612e4c2ff91bbc23db21da0d2f445e812978e
Fco Kauan <fcokauanclash@gmail.com>
2025-04-23 11:47:47 -0300

    transpondo register e login para o tailwind

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,42 +1,64 @@
 import "../Auth/Auth.module.css";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className={"login"}>
-      <div className="container">
-        <section className="login--left">
-          <h1>
-            <span className="name--project">Artfex</span> <br />o melhor para
-            sua arte
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex justify-center items-center w-full">
+        {/* REGISTER LEFT */}
+        <section className="flex-1 bg-[url('./images/tela_registro.png')] bg-cover bg-center h-screen flex flex-col items-center justify-center">
+          <h1 className="text-white mb-[6em] mr-[8em]">
+            <span className="text-8xl font-bold font-[MuseoModerno] text-[#f2994b]">
+              Art<span>fex</span>
+            </span>
+            <br />
+            <span className="text-2xl">o melhor para sua arte</span>
           </h1>
         </section>
 
-        <section className="login--rigth">
-          <div className="login--card">
-            <div className="background--form">
-              <h2>Artfex</h2>
-              <p className="subtitle">Login</p>
-              <form onSubmit={handleSubmit}>
+        {/* REGISTER RIGHT */}
+        <section className="flex-1">
+          <div className="">
+            <div className="flex justify-center items-center flex-col">
+              <h2 className="font-bold text-[#0b3c34] text-4xl mb-[1.2em]">Entrar</h2>
+              <form
+                className="flex flex-col justify-center mb-[1.5em] pb-[1.5em] border-b border-[#363636] w-[80%]"
+                onSubmit={handleSubmit}
+              >
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="email"
                   placeholder="E-mail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="password"
                   placeholder="Senha"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password || ""}
+                /> outline-none
+                <input
+                  className="opacity-90 bg-[#F2994B] hover:opacity-100 cursor-pointer transition-opacity p-[1em] pl-2 pr-2 text-white font-bold rounded-md"
+                  type="submit"
+                  value="Cadastrar"
                 />
-                <input type="submit" value="Entrar" />
+                {error && <p className="error">{error}</p>}
               </form>
-
               <p>
-                Não tem uma conta? <Link to="/register">Clique aqui</Link>
+                Já tem uma conta? <Link to="/login">Clique aqui</Link>
               </p>
             </div>
           </div>
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,6 +1,6 @@
 // import styles from "../Auth/Auth.module.css";
-import axios from "axios"
-import styles from "../tailwind/output.css"
+import axios from "axios";
+import styles from "../tailwind/output.css";
 
 //Components
 import { Link } from "react-router-dom";
@@ -33,71 +33,91 @@ const Register = () => {
       setError("As senhas precisam ser iguais");
       return;
     }
-    // envia os dados do front pro backend 
+    // envia os dados do front pro backend
     try {
       const response = axios.post("http://127.0.0.1:5000/user/criar", user);
       console.log("Usuário cadastrado com sucesso:", response.data);
     } catch (error) {
-      console.error("Erro ao cadastrar usuário:", error.response?.data || error.message);
+      console.error(
+        "Erro ao cadastrar usuário:",
+        error.response?.data || error.message
+      );
       setError("Erro ao cadastrar. Verifique os dados.");
     }
 
-    
     console.log(user);
   };
 
   return (
-    <div className="font-[MuseoModerno] ">
-      <div className="flex content-center items-center">
-
-        <section className="">
-          <h1><span className="text-9xl text-[#f2994b] font-[MuseoModerno]">Art<span>fex</span></span> <br />
-          o melhor para sua arte</h1>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex justify-center items-center w-full">
+        {/* REGISTER LEFT */}
+        <section className="flex-1 bg-[url('./images/tela_registro.png')] bg-cover bg-center h-screen flex flex-col items-center justify-center">
+          <h1 className="text-white mb-[6em] mr-[8em]">
+            <span className="text-8xl font-bold font-[MuseoModerno] text-[#f2994b]">
+              Art<span>fex</span>
+            </span>
+            <br />
+            <span className="text-2xl">o melhor para sua arte</span>
+          </h1>
         </section>
 
-        <section className="">
+        {/* REGISTER RIGHT */}
+        <section className="flex-1">
           <div className="">
-            <div className="">
-              <h2 className="bg-zinc-900">Artfex</h2>
-              <p className="">Cadastre-se</p>
-              <form onSubmit={handleSubmit}>
+            <div className="flex justify-center items-center flex-col">
+              <h2 className="font-bold text-[#0b3c34] text-4xl mb-[1.5em]">Cadastre-se</h2>
+              <form
+                className="flex flex-col justify-center mb-[1.5em] pb-[1.5em] border-b border-[#363636] w-[80%]"
+                onSubmit={handleSubmit}
+              >
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="text"
                   placeholder="Nome"
                   onChange={(e) => setName(e.target.value)}
                   value={name || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="email"
                   placeholder="E-mail"
                   onChange={(e) => setEmail(e.target.value)}
                   value={email || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="cidade"
                   placeholder="Cidade"
                   onChange={(e) => setCidade(e.target.value)}
                   value={cidade || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="estado"
                   placeholder="estado"
                   onChange={(e) => setEstado(e.target.value)}
                   value={estado || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="password"
                   placeholder="Senha"
                   onChange={(e) => setPassword(e.target.value)}
                   value={password || ""}
                 />
                 <input
+                  className="mb-[1.5em] border-b border-[#363636] pt-[0em] pr-[1em] pb-[1em] pl-[0em] outline-none"
                   type="password"
                   placeholder="Confirme sua senha"
                   onChange={(e) => setConfirmPassword(e.target.value)}
                   value={confirmPassword || ""}
                 />
-                <input type="submit" value="Cadastrar" />
+                <input
+                  className="opacity-90 bg-[#F2994B] hover:opacity-100 cursor-pointer transition-opacity p-[1em] pl-2 pr-2 text-white font-bold rounded-md"
+                  type="submit"
+                  value="Cadastrar"
+                />
                 {error && <p className="error">{error}</p>}
               </form>
               <p>
