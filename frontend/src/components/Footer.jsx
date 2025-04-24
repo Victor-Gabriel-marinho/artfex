92b79ca1884f5be88ddfe5c72313925f1864ca88
Fco Kauan <fcokauanclash@gmail.com>
2025-04-24 09:59:11 -0300

    finalizando a pagina de catalogo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,25 @@
-
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return(
-    <footer className="bg-blue-500">
-      <h1>ArtFex</h1>
-      <div className="bg-[url('../Home/images/instagram.png')]"></div>
+  return (
+    <footer className="bg-[#082621] h-[200px] flex justify-center flex-col text-center">
+      <div className="flex justify-around w-full h-full items-center m-auto flex-wrap">
+        <h1 className="text-white font-[MuseoModerno] text-2xl">ArtFex</h1>
+        <ul>
+          <li>
+            <div className="bg-[url('./images/instagram.png')] bg-cover w-[20px] h-[20px] cursor-pointer mb-[0.5em]"></div>
+          </li>
+          <li>
+            <div className="bg-[url('./images/email.png')] bg-cover w-[20px] h-[20px] cursor-pointer mb-[0.5em]"></div>
+          </li>
+          <li>
+            <div className="bg-[url('./images/telefone.png')] bg-cover w-[20px] h-[20px] cursor-pointer mb-[0.5em]"></div>
+          </li>
+        </ul>
+      </div>
+      <span className="w-full bg-[#F2994B] h-auto text-center text-white p-2"> &copy; 2025 ArtFex. Todos os direitos reservados</span>
     </footer>
   );
-}
+};
 
 export default Footer;
