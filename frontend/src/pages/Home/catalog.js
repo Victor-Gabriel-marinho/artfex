dcdc2013ec0fb2c2abce796cf75471411300cfd7
Victor <victorgab0802@gmail.com>
2025-04-24 22:37:56 -0300

    terminando a requisição dos produtos

--- frontend/src/pages/Home/catalog.js
+++ frontend/src/pages/Home/catalog.js
@@ -1,21 +1,29 @@
-import { Link } from "react-router-dom";
-
+// import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios"
 import styles from "../tailwind/output.css";
 
 // Componentes
 import NavBar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
-// const get_prod = (_,res) => {
-//   try{
-//     const response = axios.get('http://127.0.0.1:5000/produtos/produtos');
 
-    
-//   }
-// }
-//Hooks
 
 const Catalog = () => {
+  //states
+  const [produtos, setprodutos] = useState([])
+  //Hooks
+  useEffect(() => {
+    axios
+    .get('http://127.0.0.1:5000/produtos/produtos')
+    .then(Response => {
+      setprodutos(Response.data)
+      console.log(Response.data)
+    }).catch(Error => {
+      console.log("erro ao fazer requisição")
+    })
+  }, [])
+
   return (
     <div>
       <header className="h-130 pt-1 bg-[url('./images/img_bg_catalog.png')]">
@@ -38,16 +46,19 @@ const Catalog = () => {
           </form>
 
           <div className="flex flex-wrap gap-10 justify-center">
-            <div className="w-[300px] h-[350px]">
-              <div className=" w-full h-full max-h-[300px] bg-[url('./images/artesãos.png')] bg-cover rounded-xl"></div>
+            {produtos.map((produtos) =>(
+            <div className="w-[300px] h-[350px]" key={produtos.id}>
+              <img 
+              src={produtos.url}
+              className=" w-full h-full max-h-[300px] bg-cover rounded-xl"/>
               <div className="flex justify-between pl-2 pr-2">
                 <span className="text-gray-500 font">Categoria</span>
                 <span className="text-[#F2994B] font-bold font-[Poppins]">
                   R$100,00
                 </span>
               </div>
-              <p className="font-bold font-[Poppins] pl-2">Name</p>
-            </div>
+              <p className="font-bold font-[Poppins] pl-2">{produtos.nome}</p>
+            </div>))}
           </div>
         </section>
       </main>
