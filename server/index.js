21a59676e20680204d1375f843341bb741d4d16f
Victor <victorgab0802@gmail.com>
2025-06-14 11:59:13 -0300

    ajustando para o deploy

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import userRoutes from "./routers/user.js";
 import router from "./routers/produtos.js";
-import "dotenv/config";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
-// iniciando cors e usando json
+// Configurações
 app.use(cors());
 app.use(express.json());
 
-
-//usando rotas
+// Rotas
 app.use("/user", userRoutes);
-app.use("/produtos", router)
+app.use("/produtos", router);
 
-
-//executando na porta 5000
-app.listen(5000, () => console.log("Express started at http://127.0.0.1:5000"));
+// Iniciar servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
